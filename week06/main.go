2529ae783e8bda5fa63509257e3f3a1c113bf0b3
chang yeon <yeoun0852@gmail.com>
2024-10-04 17:20:32 +0900

    ver0.2

--- week06/main.go
+++ week06/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello, Go")
+	i := 13
+	var f float64 = 12.9 //f := 12.9
+
+	fmt.Printf("value i : %d, value f : %f\n", i, f)
+	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
 }
