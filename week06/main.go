028217440127503d41be82df1541e89acd196dab
chang yeon <yeoun0852@gmail.com>
2024-10-04 18:36:24 +0900

    ver0.6

--- week06/main.go
+++ week06/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9 //f := 12.9
-	c1 := 'A'
-	c2 := '김'
+	//var f float64
+	//var i interface{}
+	//var b bool
+	//var s string
 
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	fmt.Println(c1, c2)
-	fmt.Printf("%x\n", c2) //유니코드 '김'에 대한 16진수 출력
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i), reflect.TypeOf(c2))
+	//fmt.Println(f, b, s, i)
+	//fmt.Printf("%f%t%s%d\n", f, b, s, i)
+
+	i := 3
+	f := 2.7
+	fmt.Print("\n\n", f <= float64(i), "\n")
 }
