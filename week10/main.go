29f8198385c2a6ec664376d85d8b3d7090c0463c
chang yeon <yeoun0852@gmail.com>
2024-11-01 15:37:37 +0900

    Ver3.0 PrimeNumber

--- week10/main.go
+++ week10/main.go
@@ -9,9 +9,34 @@ import (
 	"strings"
 )
 
+func isPrime(n int) bool {
+	//var isPrime bool = true
+	if n <= 1 {
+		//isPrime = false
+		return false
+	} else if n == 2 {
+		//isPrime = true
+		return true
+	} else if n%2 == 0 {
+		//isPrime = false
+		return false
+	} else {
+		j := 3
+		for j*j <= n {
+			if n%j == 0 {
+				// isPrime = false
+				// break
+				return false
+			}
+			fmt.Printf("%d ", j)
+			j = j + 2
+		}
+	}
+	return true
+}
+
 func main() {
-	//fmt.Printf("%f\n", math.Sqrt(16.0))
-	fmt.Print("점수 입력 : ")
+	fmt.Print("Input number : ")
 	in := bufio.NewReader(os.Stdin)
 	i, err := in.ReadString('\n')
 	if err != nil {
@@ -23,30 +48,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var isprime bool = true
-
-	if n <= 1 {
-		isprime = false
-	} else if n == 2 {
-		isprime = true
-	} else if n%2 == 0 {
-		isprime = false
+	if isPrime(n) {
+		fmt.Printf("%d is prime number.", n)
 	} else {
-		j := 3
-		//for j <= int(math.Sqrt(float64(n))) {
-		for j*j <= n {
-			if n%j == 0 {
-				isprime = false
-				break
-			}
-			fmt.Printf("%d ", j)
-			j = j + 2
-		}
-	}
-
-	if isprime {
-		fmt.Printf("%d is prime numbers.", n)
-	} else {
-		fmt.Printf("%d is not prime numbers.", n)
+		fmt.Printf("%d is NOT prime number.", n)
 	}
 }
