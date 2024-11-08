a20107b5a3ff444c512a2be484635b1000f5cf5a
chang yeon <yeoun0852@gmail.com>
2024-11-08 17:34:00 +0900

    ver3.5 Package

--- week11/main.go
+++ week11/main.go
@@ -3,7 +3,8 @@ package main
 import "example.com/greeting/greeting"
 
 func main() {
-	greeting.Hello("Inha")
-	greeting.Hi("Inha")
+	//greeting.Hello("Inha")
+	//greeting.Hi("Inha")
+	greeting.EnglishGreetings("Inha")
 
 }
