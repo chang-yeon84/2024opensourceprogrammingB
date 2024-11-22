300f50a59e7b3599f7adc028131882897dc28dc3
chang yeon <yeoun0852@gmail.com>
2024-11-22 17:29:22 +0900

    ver4.4

--- week13/main.go
+++ week13/main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	var emptySlice []bool
-	emptySlice = make([]bool, 5)
-	fmt.Printf("%#v\n", emptySlice)
+	//emptySlice = make([]bool, 5)
+	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // slice zero value (nil), 0
+	if len(emptySlice) == 0 {
+		emptySlice = append(emptySlice, true)
+	}
+	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // []bool{true}, 1
+
 	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} // array := array literal
 	gpa_slice := gpas[1:4]                       // [4.1, 4.5, 3.9]
 	//gpa_slice[1] = 2.71
