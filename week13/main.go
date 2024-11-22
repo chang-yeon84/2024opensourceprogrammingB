8904de876a32c6f4bf49a9a75e395befe29f9612
chang yeon <yeoun0852@gmail.com>
2024-11-22 18:26:05 +0900

    ver4.5

--- week13/main.go
+++ week13/main.go
@@ -1,21 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+// func test(strs string) {
+// func test(strs ...string, i int) { // error 가변 매개변수는 반드시 마지막에 와야함
+func test(i int, strs ...string) { // 가변 매개변수 예시
+	fmt.Println(strs)
+}
 func main() {
-	var emptySlice []bool
-	//emptySlice = make([]bool, 5)
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // slice zero value (nil), 0
-	if len(emptySlice) == 0 {
-		emptySlice = append(emptySlice, true)
-	}
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // []bool{true}, 1
-
-	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} // array := array literal
-	gpa_slice := gpas[1:4]                       // [4.1, 4.5, 3.9]
-	//gpa_slice[1] = 2.71
-	gpas[2] = 2.71
-	//gpa_slice = append(gpa_slice, 4.3)
-	gpa_slice = append(gpa_slice, 4.3, 5.55)
-	fmt.Println(len(gpa_slice), gpa_slice, gpas)
+	//fmt.Println(os.Args, len(os.Args), reflect.TypeOf(os.Args))
+	slices := os.Args[1:]
+	fmt.Println(slices, slices[1])
+	test(1, "123")
+	test(-99, "abc")
+	test(55)
+	test(12, "123", "abc", "weq")
 }
