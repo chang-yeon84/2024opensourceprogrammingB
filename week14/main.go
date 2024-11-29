4ab192f79cc2079da4c65425e603f4d223afb490
chang yeon <yeoun0852@gmail.com>
2024-11-29 17:12:08 +0900

    Ver4.6 Map

---
+++ week14/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	ages := make(map[string]int)
+
+	var name string
+	var age int
+	for {
+		fmt.Print("Name :")
+		fmt.Scanln(&name)
+		if name == "q" {
+			break
+		}
+		fmt.Print("Age? ")
+		fmt.Scanln(&age)
+
+		ages[name] = age
+	}
+	for k, v := range ages {
+		fmt.Printf("%s is %d years old", k, v)
+	}
+
+}
